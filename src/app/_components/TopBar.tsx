e06c5357f00120601f6498cee7d774246b42f3df
Anderson R. Román <andersonegoistisch@gmail.com>
2024-04-22 12:38:03 -0400

    uploadthing added

--- src/app/_components/TopBar.tsx
+++ src/app/_components/TopBar.tsx
@@ -1,17 +1,31 @@
+"use client";
+
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { UploadButton } from "../utils/uploadthings";
+import { useRouter } from "next/navigation";
 
 export function TopBar() {
+  const router = useRouter();
+
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
       <div>Gallery</div>
 
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
+      <div className="flex flex-row">
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
 
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
+        <SignedIn>
+          <UploadButton
+            endpoint="imageUploader"
+            onClientUploadComplete={() => {
+              router.refresh();
+            }}
+          />
+          <UserButton />
+        </SignedIn>
+      </div>
     </nav>
   );
 }
